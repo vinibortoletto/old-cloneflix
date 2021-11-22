15e4c76a50a925d36366b8be25b8b0f93a4fb073
Vinicius Bortoletto <47364504+vinibortoletto@users.noreply.github.com>
2021-11-21 21:32:45 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { GlobalStyles } from './helpers/styles/GlobalStyles'
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <div>App</div>
+    </>
+  )
+}
---
+++ src/helpers/styles/GlobalStyles.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components/macro'
+import 'normalize.css'
+
+export const GlobalStyles = createGlobalStyle`
+
+  body {
+    background-color: #212121;
+    color: white;
+    font-family: sans-serif;
+  }
+`
