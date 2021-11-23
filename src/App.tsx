ab4e6ce76afd30174af5d30bbd94ed7e53706267
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2021-11-23 16:42:21 -0300

    add: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useLocation } from 'react-router'
+import { AppRoutes } from './routes'
+
+// Styles
 import { GlobalStyles } from './helpers/styles/GlobalStyles'
+import { getBgColor } from './helpers/styles/getBgColor'
 
 export default function App() {
+  const pathname = useLocation().pathname
+
   return (
     <>
-      <GlobalStyles />
-      <div>App</div>
+      <GlobalStyles bgColor={getBgColor(pathname)} />
+      <AppRoutes />
+      <footer></footer>
     </>
   )
 }
