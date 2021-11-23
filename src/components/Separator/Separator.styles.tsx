c47d27bad2518ad4ed7d28b503daffc10626b1fb
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2021-11-23 16:38:26 -0300

    add: separator component

---
+++ src/components/Separator/Separator.styles.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components/macro';
+import { vars } from '../../helpers/styles/variables';
+
+export const Container = styled.div`
+  background-color: ${vars.color.darkGrey};
+  width: 100%;
+  height: 0.5rem;
+`;
---
+++ src/components/Separator/Separator.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './Separator.styles';
+
+export default function Separator() {
+  return (
+    <>
+      <S.Container />
+    </>
+  );
+}
