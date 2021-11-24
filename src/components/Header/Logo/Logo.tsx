012c389acd14b5532679bea21689204bb5b618a5
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2021-11-24 17:58:38 -0300

    add: link to logo

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import * as S from './Logo.styles';
 import logo from '../../../images/misc/logo.png';
+import { Link } from 'react-router-dom';
 
 export default function Logo() {
   return (
     <>
-      <S.Container src={logo} alt="cloneflix logo" />
+      <Link to="/">
+        <S.Container src={logo} alt="cloneflix logo" />
+      </Link>
     </>
   );
 }
